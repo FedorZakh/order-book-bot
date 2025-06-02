f22f74b56a1a8e6f22c61efe7b39b20cf52341fa
Fedor Zakharov <jurfin888@gmail.com>
2025-06-02 19:47:33 +0300

    change bot_start, add for coin

--- app/bot/bot_start.py
+++ app/bot/bot_start.py
@@ -16,14 +16,65 @@ params = {
     "limit": 200,
 }
 
-# parameter = "continue"
+
 parameter = os.getenv("PARAMETER")
 
 
-def order_bot():
-    response = requests.get(url, params=params)
-    count = 0
+# def order_bot():
+#
+#     response = requests.get(url, params=params)
+#     count = 0
 
+#     with open(".env", "w") as file:
+#         file.writelines('PARAMETER="continue"\n')
+
+#     if parameter == "start":
+#         db.delete_database()
+#         db.create_table_orders()
+#         while True:
+#             if response.status_code == 200:
+#                 data = response.json()
+#                 bids = data["result"]["b"]
+#                 asks = data["result"]["a"]
+#                 name = data["result"]["s"]
+#                 min_size = 0.05
+#                 large_bids = [order for order in bids if float(order[1]) >= min_size]
+#                 large_asks = [order for order in asks if float(order[1]) >= min_size]
+#                 for i in large_bids:
+#                     db.add_order_buy(name, i[1], i[0])
+#                 for i in large_asks:
+#                     db.add_order_sell(name, i[1], i[0])
+
+#             count += 1
+#             print(count)
+#             time.sleep(5)
+
+#     elif parameter == "continue":
+#         print("started block two")
+#         while True:
+#             if response.status_code == 200:
+#                 data = response.json()
+#                 bids = data["result"]["b"]
+#                 asks = data["result"]["a"]
+#                 name = data["result"]["s"]
+#                 min_size = 0.05
+#                 large_bids = [order for order in bids if float(order[1]) >= min_size]
+#                 large_asks = [order for order in asks if float(order[1]) >= min_size]
+#                 for i in large_bids:
+#                     db.add_order_buy(name, i[1], i[0])
+#                 for i in large_asks:
+#                     db.add_order_sell(name, i[1], i[0])
+
+#             count += 1
+#             print(count)
+#             time.sleep(5)
+#     else:
+#         print("Ошибка ввода параметров запуска")
+symbols = ["BTCUSDT", "SOLUSDT"]
+
+
+def order_bot():
+    count = 0
     with open(".env", "w") as file:
         file.writelines('PARAMETER="continue"\n')
 
@@ -31,41 +82,49 @@ def order_bot():
         db.delete_database()
         db.create_table_orders()
         while True:
-            if response.status_code == 200:
-                data = response.json()
-                bids = data["result"]["b"]
-                asks = data["result"]["a"]
-                name = data["result"]["s"]
-                min_size = 0.05
-                large_bids = [order for order in bids if float(order[1]) >= min_size]
-                large_asks = [order for order in asks if float(order[1]) >= min_size]
+            for s in symbols:
+                params["symbol"] = s
+                response = requests.get(url, params=params)
+                if response.status_code == 200:
+                    data = response.json()
+                    bids = data["result"]["b"]
+                    asks = data["result"]["a"]
+                    name = data["result"]["s"]
+                    min_size = 0.5
+                    large_bids = [
+                        order for order in bids if float(order[1]) >= min_size
+                    ]
+                    large_asks = [
+                        order for order in asks if float(order[1]) >= min_size
+                    ]
                 for i in large_bids:
                     db.add_order_buy(name, i[1], i[0])
                 for i in large_asks:
                     db.add_order_sell(name, i[1], i[0])
-
             count += 1
             print(count)
-            time.sleep(5)
-
+            time.sleep(60)
     elif parameter == "continue":
-        print("started block two")
         while True:
-            if response.status_code == 200:
-                data = response.json()
-                bids = data["result"]["b"]
-                asks = data["result"]["a"]
-                name = data["result"]["s"]
-                min_size = 0.05
-                large_bids = [order for order in bids if float(order[1]) >= min_size]
-                large_asks = [order for order in asks if float(order[1]) >= min_size]
+            for s in symbols:
+                params["symbol"] = s
+                response = requests.get(url, params=params)
+                if response.status_code == 200:
+                    data = response.json()
+                    bids = data["result"]["b"]
+                    asks = data["result"]["a"]
+                    name = data["result"]["s"]
+                    min_size = 0.5
+                    large_bids = [
+                        order for order in bids if float(order[1]) >= min_size
+                    ]
+                    large_asks = [
+                        order for order in asks if float(order[1]) >= min_size
+                    ]
                 for i in large_bids:
                     db.add_order_buy(name, i[1], i[0])
                 for i in large_asks:
                     db.add_order_sell(name, i[1], i[0])
-
             count += 1
             print(count)
-            time.sleep(5)
-    else:
-        print("Ошибка ввода параметров запуска")
+            time.sleep(60)
